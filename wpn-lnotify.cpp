4f9d81434ea44daa7e305cf4a79ce0497a086999
andrei <andrei.i.ivanov@gmail.com>
2018-04-30 15:50:25 +0900

    First release

--- wpn-lnotify.cpp
+++ wpn-lnotify.cpp
@@ -3,6 +3,8 @@
 #include <libnotify/notify.h>
 #include "wpn-lnotify.h"
 
+static const std::string APP_NAME("wpn");
+
 int desktopNotify
 (
 	const std::string &title,
@@ -14,5 +16,20 @@ int desktopNotify
 	const std::string &extra
 )
 {
+	notify_init(APP_NAME.c_str());
+    NotifyNotification* notification = 
+		notify_notification_new(
+			title.empty() ? NULL : title.c_str(),
+			body.empty() ? NULL : body.c_str(),
+			icon.empty() ? NULL : icon.c_str());
+	if (timeout <= 0)
+		timeout = 10000;	// 10 seconds
+    notify_notification_set_timeout(notification, timeout); 
+
+    if (!notify_notification_show(notification, NULL)) 
+    {
+        return -1;
+    }
+    // notify_notification_close(notification, NULL);
 	return 0;
 }
