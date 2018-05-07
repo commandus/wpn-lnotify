a46a93eedfb7466a9dd934382c03bc5a8340a3ba
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2018-05-07 13:56:23 +0900

    Receive message

--- test/main.cpp
+++ test/main.cpp
@@ -3,21 +3,6 @@
 #include <dlfcn.h>
 #include "../wpn-notify.h"
 
-typedef 
-	bool (*desktopNotifyFunc)
-	(
-		const std::string &title,
-		const std::string &body,
-		const std::string &icon,		///< Specifies an icon filename or stock icon to display.
-		const std::string &sound,		///< sound file name
-		const std::string &link,		///< click action
-		const std::string &linkType,	///< click action
-		int urgency, 					///< low- 0, normal, critical
-		int timeout, 					///< timeout in milliseconds at which to expire the notification.
-		const std::string &category,
-		const std::string &extra
-	);
-
 int main (int argc, char **argv)
 {
 	std::string title;
@@ -46,7 +31,9 @@ int main (int argc, char **argv)
 	}
   
 	desktopNotifyFunc desktopNotify = (desktopNotifyFunc) dlsym(so, "desktopNotify");
-	bool r = (*desktopNotify)(title, body, "", "", "", "", 0, 0, "", "") ? 0 : -1;
+	bool r = (*desktopNotify)(
+		"", "", "", 0,
+		"", title, body, "", "", "", "", 0, 0, "", "", "") ? 0 : -1;
 	dlclose(so);
 	return r;
 	
