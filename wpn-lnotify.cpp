395f55be7b852da06a13a3587c0105cd92707ccb
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2018-05-07 16:25:59 +0900

    Receive message

--- wpn-lnotify.cpp
+++ wpn-lnotify.cpp
@@ -9,36 +9,28 @@ bool desktopNotify
 (
 	const std::string &persistent_id,
 	const std::string &from,					///< e.g. BDOU99-h67HcA6JeFXHbSNMu7e2yNNu3RzoMj8TM4W88jITfq7ZmPvIM1Iv-4_l2LxQcYwhqby2xGpWwzjfAnG4
-	const std::string &subtype,
+	const std::string &appName,
+	const std::string &appId,
 	int64_t sent,
 
-	const std::string &authorizedEntity,		///< e.g. 246829423295
-	const std::string &title,
-	const std::string &body,
-	const std::string &icon,					///< Specifies an icon filename or stock icon to display.
-	const std::string &sound,					///< sound file name
-	const std::string &link,					///< click action
-	const std::string &linkType,				///< click action
-	int urgency, 								///< low- 0, normal, critical
-	int timeout,	 							///< timeout in milliseconds at which to expire the notification.
-	const std::string &category,
-	const std::string &extra,
-	const std::string &data					///< extra data in JSON format
+	const NotifyMessage *request,
+	NotifyMessage *reply
 )
 {
-
+	if (!request)
+		return false;
 	notify_init(APP_NAME.c_str());
     NotifyNotification *notification = notify_notification_new
     (
-		title.empty() ? NULL : title.c_str(),
-		body.empty() ? NULL : body.c_str(),
-		icon.empty() ? NULL : icon.c_str()
+		request->title.empty() ? NULL : request->title.c_str(),
+		request->body.empty() ? NULL : request->body.c_str(),
+		request->icon.empty() ? NULL : request->icon.c_str()
 	);
 	if (!notification)
 		return false;
-    notify_notification_set_timeout(notification, timeout <= 0 ? 10000 : timeout); 	// 10 seconds
+    notify_notification_set_timeout(notification, request->timeout <= 0 ? 10000 : request->timeout); 	// 10 seconds
 
     bool r = notify_notification_show(notification, NULL);
     g_object_unref(G_OBJECT(notification));
-	return r;
+	return false;
 }
--- wpn-stdout.cpp
+++ wpn-stdout.cpp
@@ -8,25 +8,18 @@ bool desktopNotify
 (
 	const std::string &persistent_id,
 	const std::string &from,					///< e.g. BDOU99-h67HcA6JeFXHbSNMu7e2yNNu3RzoMj8TM4W88jITfq7ZmPvIM1Iv-4_l2LxQcYwhqby2xGpWwzjfAnG4
-	const std::string &subtype,
+	const std::string &appName,
+	const std::string &appId,
 	int64_t sent,
 
-	const std::string &authorizedEntity,		///< e.g. 246829423295
-	const std::string &title,
-	const std::string &body,
-	const std::string &icon,					///< Specifies an icon filename or stock icon to display.
-	const std::string &sound,					///< sound file name
-	const std::string &link,					///< click action
-	const std::string &linkType,				///< click action
-	int urgency, 								///< low- 0, normal, critical
-	int timeout,				 				///< timeout in milliseconds at which to expire the notification.
-	const std::string &category,
-	const std::string &extra,
-	const std::string &data					///< extra data in JSON format
+	const NotifyMessage *request,
+	NotifyMessage *reply
 )
 {
+	if (!request)
+		return false;
 	std::cout 
-		<< (title.empty() ? "" : title) << "\t"
-		<< (body.empty() ? "" : body) << std::endl;
-	return true;
+		<< (request->title.empty() ? "" : request->title) << "\t"
+		<< (request->body.empty() ? "" : request->body) << std::endl;
+	return false;
 }
